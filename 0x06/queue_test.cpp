92441a4fa8d9a4db4f33f92ea56fc56fb4be6dd5
gosu <jonathan.jh.kim@gmail.com->
2025-12-14 03:53:35 +0900

    큐,덱,괄호쌍 공부함

--- 0x06/queue_test.cpp
+++ 0x06/queue_test.cpp
@@ -3,22 +3,29 @@ using namespace std;
 
 const int MX = 1000005;
 int dat[MX];
-int head = 0, tail = 0;
+int head = 0, tail = 0; 
+// 헤드는 가장 앞 원소 인덱스, 꼬리는 가장 뒤 인덱스+1
+// 빈 큐에는 헤드=꼬리=0
+// 큐의 크기=꼬리-헤드
+// 원형 큐도 있음
+// 이건 그냥 선형 큐
 
 void push(int x){
-
+  dat[tail]=x;
+  tail++;
 }
 
 void pop(){
-
+  if(tail>head)
+  head++;
 }
 
-int front(){
-
+int front(){ // 제일 앞 원소 반환
+  return dat[head];
 }
 
-int back(){
-
+int back(){ // 제일 뒤 원소 반환
+  return dat[tail-1];
 }
 
 void test(){
--- 0x07/deque_test.cpp
+++ 0x07/deque_test.cpp
@@ -4,28 +4,34 @@ using namespace std;
 const int MX = 1000005;
 int dat[2*MX+1];
 int head = MX, tail = MX;
+// head: 가장 앞 원소 인덱스, tail: 가장 뒤 원소 인덱스+1
+// 그냥 선형 덱 구현
 
 void push_front(int x){
-  
+  dat[head-1]=x;
+  head--;
 }
 
 void push_back(int x){
-  
+  dat[tail]=x;
+  tail++;
 }
 
 void pop_front(){
-  
+  if(head!=tail) head++;
 }
 
 void pop_back(){
+  if(head!=tail) tail--;
   
 }
 
 int front(){
-  
+  return dat[head];
 }
 
 int back(){
+  return dat[tail-1];
   
 }
 
